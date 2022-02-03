2d8b0a14cf76bfbc369a35ce931ec481cf221080
Manikandan Rajendran <r.manikandan.king@gmail.com>
2022-02-03 16:51:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
     
 setuptools.setup(
     name='pythonCommons',
@@ -10,7 +7,6 @@ setuptools.setup(
     author='Manikandan Rajendran',
     author_email='manikandan.rajendran@betwaygroup.com',
     description='Testing installation of Package',
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/ManikandanRajendran/pythonCommons',
     project_urls = {
