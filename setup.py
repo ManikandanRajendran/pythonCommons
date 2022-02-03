dc5bb87b5cae9821f2013e13567ad8695985e5dc
Manikandan Rajendran <r.manikandan.king@gmail.com>
2022-02-03 16:51:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setuptools.setup(
     name='pythonCommons',
     version='0.0.1',
