c0f22991ca6659ceb70fc296083881f499e8838d
Manikandan Rajendran <r.manikandan.king@gmail.com>
2022-02-04 19:21:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-import setuptools
+from setuptools import setup
 
-    
-setuptools.setup(
-    name='pythonCommons',
-    version='0.0.1',
-    author='Manikandan Rajendran',
-    author_email='manikandan.rajendran@betwaygroup.com',
-    description='Testing installation of Package',
-    long_description_content_type="text/markdown",
-    url='https://github.com/ManikandanRajendran/pythonCommons',
-    project_urls = {
-        "Bug Tracker": "https://github.com/ManikandanRajendran/pythonCommons/issues"
-    },
-    license='MIT',
-    packages=['pythonCommons'],
-    install_requires=['requests'],
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="pythonCommons",
+    install_requires=[
+        "requests",
+    ]
 )
+
